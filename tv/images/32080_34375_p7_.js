0219412bb50463f9251f5ac12d8d3bf67002c8bb
fjwzp <54281794+fjwzp@users.noreply.github.com>
2024-06-23 01:43:57 +0800

    Add files via upload

---
+++ tv/images/32080_34375_p7_.js
@@ -0,0 +1,3 @@
+var c_width_vodone=255;var c_height_vodone=235;
+var sid_vodone="p7";var cid_vodone="2";var oid_vodone="32080|34375|21203|0|"; var urls="http://tv.ka88.com,";
+document.writeln('<script src="http://busjs.vodone.cn/liveuni/liveuni.js" charset="UTF-8"><\/script>');
